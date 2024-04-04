b21ffbb1cc054f4265a635ad4880609ec947be73
JP Grusling <jp.grusling@fusionary.com>
2024-04-03 20:58:14 -0400

    change eslint rules

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -1,43 +1,79 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   extends: [
-    "turbo",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
+    'turbo',
+    'eslint:recommended',
+    'plugin:storybook/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:@shopify/esnext',
+    'plugin:@shopify/typescript',
+    'plugin:@shopify/typescript-type-checking',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:@shopify/prettier',
   ],
   env: {
     es2022: true,
     node: true,
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: { project: true },
-  plugins: ["@typescript-eslint", "import"],
+  plugins: ['@typescript-eslint', 'import', 'unused-imports', 'prettier'],
   rules: {
-    "turbo/no-undeclared-env-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    'import/first': ['error'],
+    'import/no-extraneous-dependencies': ['off'],
+    'import/newline-after-import': ['error'],
+    'import/no-duplicates': ['error'],
+    'import/prefer-default-export': ['off'],
+    'import/order': ['off'],
+    'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+
+    'no-console': ['warn'],
+
+    'tailwindcss/no-custom-classname': ['off'],
+
+    'unused-imports/no-unused-imports-ts': ['error'],
+
+    '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
     ],
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      { prefer: "type-imports", fixStyle: "separate-type-imports" },
-    ],
-    "@typescript-eslint/no-misused-promises": [
+    '@typescript-eslint/no-empty-function': ['warn'],
+    '@typescript-eslint/no-explicit-any': ['error'],
+    '@typescript-eslint/no-misused-promises': [
       2,
       { checksVoidReturn: { attributes: false } },
     ],
-    "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: false, classes: false },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
   ignorePatterns: [
-    "**/*.config.js",
-    "**/*.config.cjs",
-    "**/.eslintrc.cjs",
-    ".next",
-    "dist",
-    "pnpm-lock.yaml",
+    '**/*.config.js',
+    '**/*.config.cjs',
+    '**/.eslintrc.cjs',
+    '.next',
+    'dist',
+    'pnpm-lock.yaml',
   ],
   reportUnusedDisableDirectives: true,
-};
+  overrides: [
+    {
+      files: ['**/*.json', '**/*.json5', '**/*.jsonc'],
+      parser: 'jsonc-eslint-parser',
+      extends: ['plugin:jsonc/all', 'plugin:jsonc/prettier'],
+      rules: {},
+    },
+  ],
+}
 
-module.exports = config;
+module.exports = config
--- tooling/eslint/react.js
+++ tooling/eslint/react.js
@@ -1,24 +1,37 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
-  extends: [
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-  ],
+  extends: ['plugin:@shopify/react', 'plugin:jsx-a11y/recommended'],
   rules: {
-    "react/prop-types": "off",
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        labelComponents: ['FormLabel'],
+        labelAttributes: ['children'],
+        depth: 3,
+      },
+    ],
+
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': ['off'],
   },
   globals: {
-    React: "writable",
+    React: 'writable',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   env: {
     browser: true,
   },
-};
+}
 
-module.exports = config;
+module.exports = config
