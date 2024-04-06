73eaefbe44f18b2068da04e0743209f9cdaeede4
JP Grusling <jp.grusling@fusionary.com>
2024-04-06 09:58:09 -0400

    updating heading variants

--- packages/ui/src/components/Heading/heading.variants.ts
+++ packages/ui/src/components/Heading/heading.variants.ts
@@ -3,17 +3,19 @@ import { type ClassProp } from 'class-variance-authority/types'
 
 import { cn } from '~/utils/cn'
 
+const headlines = ['text-textHeadline', 'font-normal']
+
 export const headingVariants = cva(
   ['font-bold', 'text-black', 'dark:text-white'],
   {
     variants: {
       as: {
-        h1: ['text-6xl'],
-        h2: ['text-5xl'],
-        h3: ['text-4xl'],
-        h4: ['text-3xl'],
-        h5: ['text-2xl'],
-        h6: ['text-xl'],
+        h1: [...headlines, 'text-[64px] leading-[76.8px]'],
+        h2: [...headlines, 'text-[56px] leading-[67.2px]'],
+        h3: [...headlines, 'text-[38px] leading-[45.6px]'],
+        h4: [...headlines, 'text-[28px] leading-[33.6px]'],
+        h5: [...headlines, 'text-[20px] leading-[24px]'],
+        h6: [...headlines, 'text-[18px] leading-[21.6px]'],
       },
       align: {
         center: ['text-center'],
