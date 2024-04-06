1e284eb8731b5026dc32dd13037c262855450489
JP Grusling <jp.grusling@fusionary.com>
2024-04-06 10:12:02 -0400

    simplify heading

--- packages/ui/src/components/Heading/heading.variants.ts
+++ packages/ui/src/components/Heading/heading.variants.ts
@@ -3,29 +3,24 @@ import { type ClassProp } from 'class-variance-authority/types'
 
 import { cn } from '~/utils/cn'
 
-const headlines = ['text-textHeadline', 'font-normal']
-
-export const headingVariants = cva(
-  ['font-bold', 'text-black', 'dark:text-white'],
-  {
-    variants: {
-      display: {
-        h1: [...headlines, 'text-[64px] leading-[76.8px]'],
-        h2: [...headlines, 'text-[56px] leading-[67.2px]'],
-        h3: [...headlines, 'text-[38px] leading-[45.6px]'],
-        h4: [...headlines, 'text-[28px] leading-[33.6px]'],
-        h5: [...headlines, 'text-[20px] leading-[24px]'],
-        h6: [...headlines, 'text-[18px] leading-[21.6px]'],
-        base: [...headlines, 'text-[16px] leading-[22.4px]'],
-        small: [...headlines, 'text-[14px] leading-[16.8px]'],
-        table: [...headlines, 'text-[14px] leading-[16.8px]'],
-      },
-    },
-    defaultVariants: {
-      display: 'h1',
+export const headingVariants = cva(['text-textHeadline', 'font-normal'], {
+  variants: {
+    display: {
+      h1: ['text-[64px] leading-[76.8px]'],
+      h2: ['text-[56px] leading-[67.2px]'],
+      h3: ['text-[38px] leading-[45.6px]'],
+      h4: ['text-[28px] leading-[33.6px]'],
+      h5: ['text-[20px] leading-[24px]'],
+      h6: ['text-[18px] leading-[21.6px]'],
+      base: ['text-[16px] leading-[22.4px]'],
+      small: ['text-[14px] leading-[16.8px]'],
+      table: ['text-[14px] leading-[16.8px]'],
     },
   },
-)
+  defaultVariants: {
+    display: 'h1',
+  },
+})
 
 export type HeadingVariantsProps = VariantProps<typeof headingVariants>
 
